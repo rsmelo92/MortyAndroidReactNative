2a3257b807d84fac391a2e59fe04d3b4eb5853d0
Rafael Melo <rsmelo_@hotmail.com>
2019-03-16 16:34:44 -0300

    Fix

--- App.js
+++ App.js
@@ -110,7 +110,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
   },
   activityIndicator: {
     alignItems: "center",
